853c827297639d4d5e0185158774b856d89c7f82
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2021-12-12 17:02:12 -0500

    Completed Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const Hq =42;
+const feet =264;
+
+function distanceFromHqInBlocks(num){
+    if (num > 42) {
+        return num - Hq;   
+    }else if (num < 42) {
+        return Hq - num;
+    }
+}
+
+function distanceFromHqInFeet(num){
+    return distanceFromHqInBlocks(num)* feet;
+}
+
+function distanceTravelledInFeet(Hq,num2){
+    if (num2 > Hq){
+        return(num2 - Hq)* feet;
+    }else {
+        return(Hq - num2)*feet;
+    }
+}
+
+function calculatesFarePrice(start,destination){
+    const travel = distanceTravelledInFeet(start,destination)
+    if (travel < 400){
+        return(0)
+    }else if (travel > 400 && travel < 2000){
+        return (travel - 400) * .02 
+    }else if (travel > 2000 && travel < 2500){
+        return(25);
+    }else{
+        return("cannot travel that far")
+    }    
+  
+}
